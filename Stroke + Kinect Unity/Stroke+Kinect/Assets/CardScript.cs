4c0eca3625caafc8907cfa6b0fc2804d15602d20
unknown <lightnarcissus@gmail.com>
2016-02-18 10:16:50 -0500

    Made simple conductor and card matching game upto 3 levels in Unity

---
+++ Stroke + Kinect Unity/Stroke+Kinect/Assets/CardScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardScript : MonoBehaviour {
+
+    public bool isPicking = false;
+    private Vector3 tempPos;
+    public GameObject levelManager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isPicking)
+        {
+            tempPos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(tempPos.x, tempPos.y,0f);
+        }
+	
+	}
+
+    void OnMouseDown()
+    {
+        isPicking = true;
+    }
+    void OnMouseUp()
+    {
+        isPicking = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log(col.gameObject.name);
+        if(col.gameObject.name==gameObject.name)
+        {
+            levelManager.GetComponent<CardLevelManager>().CorrectMatch();
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Stroke + Kinect Unity/Stroke+Kinect/Assets/PointerCollide.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerCollide : MonoBehaviour {
+
+    public GameObject[] activeImg;
+    public AudioSource source;
+    public AudioClip[] orcClips;
+    private int activeArc = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseOver()
+    {
+        switch(gameObject.name)
+        {
+            case "LeftArc":
+                activeImg[activeArc].SetActive(false);
+                activeImg[0].SetActive(true);
+                activeArc = 0;
+                source.clip = orcClips[0];
+                if (!source.isPlaying)
+                    source.Play();
+                break;
+            case "CentreArc":
+                activeImg[activeArc].SetActive(false);
+                activeImg[1].SetActive(true);
+                source.clip = orcClips[1];
+                activeArc = 1;
+                if(!source.isPlaying)
+                    source.Play();
+                break;
+            case "RightArc":
+                activeImg[activeArc].SetActive(false);
+                activeImg[2].SetActive(true);
+                source.clip = orcClips[2];
+                activeArc = 2;
+                //Debug.Log("in right");
+                if (!source.isPlaying)
+                    source.Play();
+                break;
+        }
+   
+    }
+}
