c9672ff3e0b794d75551410f28ef240f7835d0ad
Ansh <lightnarcissus@gmail.com>
2016-03-03 10:16:34 -0500

    Game page and Title page are now in the same scene

---
+++ Stroke + Kinect Unity/Stroke+Kinect/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public KinectManager kinManager;
+	public GameObject gamePage;
+	public GameObject titlePage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (kinManager.avatarControllers.Count > 0) {
+			titlePage.SetActive (false);
+			gamePage.SetActive (true);
+		} else {
+			titlePage.SetActive (true);
+			gamePage.SetActive (false);
+		}
+	
+	}
+}
