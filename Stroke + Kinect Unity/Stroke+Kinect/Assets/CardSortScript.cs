0eea96729673b810c8f135688ba874e3509a5c90
eozinche <ec1958@nyu.edu>
2016-03-14 00:53:49 -0400

    Added Lv4 & Sparkle to the card game

---
+++ Stroke + Kinect Unity/Stroke+Kinect/Assets/CardSortScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardSortScript : MonoBehaviour {
+			
+		public bool isPicking = false;
+		private Vector3 tempPos;
+		GameObject levelManager;
+		
+		void Start () {
+			levelManager = GameObject.Find("LevelManager");
+		}
+		
+		void Update () {
+			if(isPicking) {
+				tempPos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				transform.position = new Vector3(tempPos.x, tempPos.y, 0f);
+			}
+		}
+		
+		void OnMouseDown() {
+			isPicking = true;
+		}
+		
+		void OnMouseUp() {
+			isPicking = false;
+		}
+		
+		void OnTriggerEnter2D(Collider2D col)
+		{
+			//Debug.Log(col.gameObject.GetInstanceID());
+			
+			if (gameObject.tag == "Lv4Colors" && col.gameObject.name == "Colors") {
+				levelManager.GetComponent<CardLevelManager> ().CorrectMatch ();
+				Destroy (gameObject);
+			}
+
+			if (gameObject.tag == "Lv4Nums" && col.gameObject.name == "Numbers") {
+				levelManager.GetComponent<CardLevelManager> ().CorrectMatch ();
+				Destroy (gameObject);
+			}
+
+			if (gameObject.tag == "Lv4Pets" && col.gameObject.name == "Pets") {
+				levelManager.GetComponent<CardLevelManager> ().CorrectMatch ();
+				Destroy (gameObject);
+			}
+			
+			if (gameObject.tag == "Lv4Flowers" && col.gameObject.name == "Flowers") {
+				levelManager.GetComponent<CardLevelManager> ().CorrectMatch ();
+				Destroy (gameObject);
+			}
+		}
+	}
+	
